0f0b3a9bd29007fdb2b7f7e7bdfacca87748685f
Inomjon12 <inomjonismanaliev@gmail.com>
2022-11-15 18:33:12 +0300

    js

--- js/script.js
+++ js/script.js
@@ -10,17 +10,21 @@ window.addEventListener("DOMContentLoaded", () => {
   line1.addEventListener("click", () => {
     slider.style.transform = "translateX(0)";
     active.style.top = 0;
+    container.style.background = "#c4d719";
   });
   line2.addEventListener("click", () => {
     slider.style.transform = "translateX(-25%)";
     active.style.top = "80px";
+    container.style.background = "#195bd7";
   });
   line3.addEventListener("click", () => {
     slider.style.transform = "translateX(-50%)";
     active.style.top = "160px";
+    container.style.background = "#19d762";
   });
   line4.addEventListener("click", () => {
     slider.style.transform = "translateX(-75%)";
     active.style.top = "240px";
+    container.style.background = "#d71919";
   });
 });
